795bd0d40fe029480be5eb6cecced64eb110e18f
bmirisola <bennymirisola@gmail.com>
2021-04-14 20:14:15 -0400

    removed ms and *

--- 3_traceroute_data/hw.sh
+++ 3_traceroute_data/hw.sh
@@ -1,12 +1,15 @@
 echo "cleaning up data!" 
 awk -f cleanup.awk raw_data > delay
 
+echo "cleanup part 2"
+awk -f cleanup2.awk delay > cleaned
+
 #echo "adding numbers"
 #awk -f numbered_delay.awk delay > alldata
 
 #sort data
 echo "sorting data"
-sort -n delay > sorted
+sort -n cleaned > sorted
 
 echo "converting to cdf"
 awk -f cdf_data_gen.awk sorted > cdf
